c8544294c8eeede242a0b9bd30a9aab976359971
lindolfogadelha <lindolfo_neto2010@hotmail.com>
2024-04-29 19:13:48 -0300

    segundo

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-import express  from 'express';
-import {v4} from 'uuid';
-import cors from 'cors';
-
-
+const express = require('express')
+const cors = require('cors')
 const port = 3001
 
 const app = express()
-app.use(express.json());
-app.use(cors() );
-
+const uuid = require('uuid')
+app.use(express.json())
+app.use(cors())
 
 const users = []
 
-const checkUserId = (request, response, next) =>{
+const checkUserId = (request, response, next) => {
     const { id } = request.params
-    const index = users.findIndex( user => user.id === id)
 
-    if ( index < 0){
-        return response.status(404).json({error: 'User not found'})
+    const index = users.findIndex(user => user.id === id)
+
+    if (index < 0) {
+        return response.status(404).json({ message: "User not found" })
     }
 
     request.userIndex = index
@@ -26,56 +24,46 @@ const checkUserId = (request, response, next) =>{
     next()
 }
 
+app.get('/users', (request, response) => { // buscando informações back-end
 
-app.get('/users', (request, response) =>{
+    return response.json(users)
+})
 
-    return response.json( users)
-})   
-  
-    
-app.post('/users', (request, response) =>{
+app.post('/users', (request, response) => { // criando informações back-end
 
-    const {name, age } = request.body
-    
-
-    const user = {id:uuid.v4(), name, age}
+    const { name, age } = request.body
+    const user = { id: uuid.v4(), name, age }
 
     users.push(user)
 
-    return response.status(201).json( user )
+    return response.status(201).json(user)
 })
 
-app.put('/users/:id',checkUserId, (request, response) =>{
+app.put('/users/:id', checkUserId, (request, response) => { // atualização informação back-end
 
     const { name, age } = request.body
     const index = request.userIndex
     const id = request.userId
 
-    const updateUser = {id, name, age}
+    const updateUser = { id, name, age }
 
-    
-    users[ index ] = updateUser
+    users[index] = updateUser
 
-    return response.json( updateUser)
+    return response.json(updateUser)
+
 })
 
-app.get('/users', (request, response) =>{
-
-    return response.json( users)
-}) 
-    
-    
-
-app.delete('/users/:id',checkUserId, (request, response) =>{
+app.delete('/users/:id', checkUserId, (request, response) => { // deletando informações back-end
     const index = request.userIndex
 
-    
     users.splice(index, 1)
-    return response.status(204).json()
 
-    
-})  
+    return response.status(204).json(
 
-app.listen(port , () => {
-    console.log('Server is running on port ${port}')
+    )
+})
+
+
+app.listen(port, () => {
+    console.log(`Servidor ${port} rodando 🚀`)
 })
